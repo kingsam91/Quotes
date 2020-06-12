0069526c9ea65efeefc2636c6b0fa8dfd4206ec7
kingsam91 <lambetgm@gmail.com>
2020-06-12 15:12:57 +0300

    new component for user to upload quote

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { NewQuoteComponent } from './new-quote/new-quote.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
-    QuoteDetailComponent
+    QuoteDetailComponent,
+    NewQuoteComponent
   ],
   imports: [
     BrowserModule,
